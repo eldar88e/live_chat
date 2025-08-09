b47c8e2c4361828f874eeb019c9bb243019dde7e
Eldar Eminov <em88el@gmail.com>
2025-08-09 03:17:41 +0300

    Chore(action_cable): delete domen

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_cable.allowed_request_origins = %w[https://solncevdome.net http://localhost:3000]
+  config.action_cable.allowed_request_origins = %w[http://localhost:3000]
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
