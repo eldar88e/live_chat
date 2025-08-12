d46b26baca457b85e452a35603b0611a5584dc21
Eldar Eminov <em88el@gmail.com>
2025-08-12 14:25:39 +0300

    Chore(widget): add console.log to LiveChat component

--- app/frontend/components/live_chat/LiveChat.jsx
+++ app/frontend/components/live_chat/LiveChat.jsx
@@ -3,6 +3,7 @@ import { createConsumer } from "@rails/actioncable";
 import { Smile, Send } from "lucide-react";
 
 export default function LiveChat({apiUrl: apiUrl, token: token, domain: domain, cableUrl: cableUrl}) {
+    console.log(cableUrl);
     const [isOpen, setIsOpen] = useState(false);
     const [chatId, setChatId] = useState(null);
     const [messages, setMessages] = useState([]);
