89078c45a146f81e05224206318ce0eddfd22a4b
Eldar Eminov <em88el@gmail.com>
2025-10-25 22:50:22 +0300

    Small fix

--- app/jobs/telegram_sender_job.rb
+++ app/jobs/telegram_sender_job.rb
@@ -2,6 +2,10 @@ class TelegramSenderJob < ApplicationJob
   queue_as :default
 
   def perform(**args)
-    TelegramService.call(**args)
+    TelegramService.call(
+      id: args[:id] || args['tg'],
+      msg: args[:msg] || args['msg'],
+      token: args[:token] || args['token']
+    )
   end
 end
