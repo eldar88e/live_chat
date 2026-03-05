0058173381a95353bf354e3b2e43795290f35842
Eldar Eminov <em88el@gmail.com>
2026-03-05 06:22:36 +0300

    Small fix

--- app/policies/admin/message_policy.rb
+++ app/policies/admin/message_policy.rb
@@ -14,12 +14,27 @@ module Admin
 
     private
 
+    def chat_widget
+      @chat_widget ||= record.chat.chat_widget
+    end
+
     def chat_widget_owner?
-      record.chat.chat_widget.owner_id == user.id
+      chat_widget.owner_id == user.id
     end
 
     def chat_widget_member?
-      record.chat.chat_widget.users.include?(user)
+      chat_widget.users.exists?(user.id)
+    end
+
+    class Scope < ApplicationPolicy::Scope
+      def resolve
+        if user.root?
+          scope.all
+        else
+          chat_widget_ids = user.owned_chat_widgets.ids + user.chat_widgets.ids
+          scope.joins(chat: :chat_widget).where(chat_widgets: { id: chat_widget_ids })
+        end
+      end
     end
   end
 end
